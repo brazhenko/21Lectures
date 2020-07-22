d17dc7be59377c52cd67fff98a84b407f359145d
Lommy Reznak <lreznak-@mi-i1.msk.21-school.ru>
2020-07-22 22:14:48 +0300

    +

---
+++ Algo2/task7.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Lommy Reznak on 7/22/20.
+//
+
+/*
+ * All DNA is composed of a series of nucleotides abbreviated as A, C, G, and T,
+ * for example: "ACGAATTCCG". When studying DNA, it is sometimes useful to
+ * identify repeated sequences within the DNA.
+ *
+ * Write a function to find all the 10-letter-long sequences
+ * (substrings) that occur more than once in a DNA molecule.
+ *
+ * Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+ * Output: ["AAAAACCCCC", "CCCCCAAAAA"]
+ *
+ * No ocal tests provided. If you want to test go to
+ * https://leetcode.com/problems/repeated-dna-sequences/
+ * and make your submission.
+ */
+
+vector<string> findRepeatedDnaSequences(string s)
+{
+	return {"school21"};
+}
